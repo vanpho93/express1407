80afa8c787681309630c5ab774f55b9b6a0d8f75
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-28 21:20:46 +0700

    xong

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
+app.use(express.static('./public'));
 //get post put delete
 app.get('/', (req, res) => {
     res.render('home');
